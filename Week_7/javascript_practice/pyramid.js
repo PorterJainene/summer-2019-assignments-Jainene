4a451e623c480d298cd265dc09d8e56138771350
student <student@PATC07.local>
2019-07-26 13:09:47 -0700

    pyramid notes updated

--- Week_7/javascript_practice/pyramid.js
+++ Week_7/javascript_practice/pyramid.js
@@ -1,4 +1,4 @@
-function makePyramid(length) {
+/*function makePyramid(length) {
     var line = "*";
     for (var j = 1; j <= length; j++) {
         line += " * ";
@@ -6,8 +6,52 @@ function makePyramid(length) {
     return line + "\n";
 }
 console.log(makePyramid(1));
+*/
 
+/*
+var ast1 = "*"
 
+console.log(ast1);
 
+ast1 = (ast1 + "*")
 
+console.log(ast1);
 
+for (var i=0; i<5; i++){
+    l
+}
+*/
+
+var ast1 = " ";
+var astReverse = " ";
+
+// going backwards
+
+for (var i =3; i>0; i--) {
+    console.log(i)
+    astReverse = astReverse + "*";
+
+} 
+for (var i=0; i<5; i++) {
+    ast1 = ast1 + "*";
+}
+
+/*two star loop
+astReverse = " ";
+for (var i = 2; i>0; i--) {
+    astReverse=astReverse + "*";
+}
+console.log(astReverse);
+
+*/
+
+//string builder
+astReverse = " ";
+for (var i = 3; i>0; i--) {
+    astReverse=astReverse + "*";
+
+for (var e = 10; e>0; e--)
+
+}
+
+console.log(astReverse)
