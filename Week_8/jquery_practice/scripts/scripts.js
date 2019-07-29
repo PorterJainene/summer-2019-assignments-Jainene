db12fd0e202ca6ec6ba959ebf0e0be7db698ff88
student <student@PATC07.local>
2019-07-29 13:05:21 -0700

    jQuery practice

---
+++ Week_8/jquery_practice/scripts/scripts.js
@@ -0,0 +1,14 @@
+console.log("Hello there!");
+
+$(document).ready(function ()  {
+
+    console.log("jQuery is ready!");
+
+    // css jQuery Method
+    $("p").css("font-size", "100px")
+
+});
+
+
+// something.action(    callbackfunction (){} );
+
